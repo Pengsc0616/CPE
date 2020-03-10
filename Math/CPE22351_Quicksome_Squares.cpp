9287233ae4cc87b8a98563323507b7e835d8fd85
Pengsc0616 <st282238@gmail.com>
2020-03-10 20:39:52 +0800

    add CPE22351

--- Math/CPE22351_Quicksome_Squares.cpp
+++ Math/CPE22351_Quicksome_Squares.cpp
@@ -1,1 +1,18 @@
-
+#include <iostream>
+#include <cmath>  //pow
+#include <iomanip> //setw, setfill
+using namespace std;
+void Map(int n){
+    int head, tail;
+    int divide = pow(10.0,n/2);
+    for(int i=0; i<divide; i++){
+        head=(i*i)/divide;
+        tail=(i*i)%divide;
+        if(i==head+tail)  cout << setw(n) << setfill('0') << i*i << endl;
+    }
+}
+int main(){
+    int n;
+    while(cin >> n) Map(n);
+    return 0;
+}
